b03a4b34b98896eba4c1b921ac73529dc5738193
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-11-06 01:09:52 -0800

    Update hello-rustaceans.rs

--- intro/hello-rustaceans.rs
+++ intro/hello-rustaceans.rs
@@ -1,10 +1,10 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 fn main() {
-	let stdout = stdout();
-	let message = String::from("Hello fellow Rustaceans!");
-	let width = message.chars().count();
+	let stdout: std::io::Stdout = stdout();
+	let message: String = String::from("Hello fellow Rustaceans!");
+	let width: usize = message.chars().count();
 	
-	let mut writer = BufWriter::new(stdout.lock());
+	let mut writer: BufWriter<std::io::StdoutLock> = BufWriter::new(stdout.lock());
 	say(&message, width, &mut writer).unwrap();	
 }
