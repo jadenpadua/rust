f09c56408cb2eb6028a8916ae134c11e82dcacf7
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-11-07 21:56:14 -0800

    Update palindrome-number.rs

--- string/palindrome-number.rs
+++ string/palindrome-number.rs
@@ -1,4 +1,18 @@
-impl Solution {
+impl Solution1 {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        let (mut y, mut z): (i32,i32) = (x,0);
+        while y > 0 {
+            z = z*10 + y%10;
+            y /= 10;
+        }
+        return x == z;
+    }
+}
+
+impl Solution2 {
     pub fn is_palindrome(x: i32) -> bool {
         return x.to_string().chars().rev().eq(x.to_string().chars());
     }
