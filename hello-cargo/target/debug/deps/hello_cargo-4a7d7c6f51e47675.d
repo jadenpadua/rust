49f292fcb941c7ff5eb08172d6855b0779b30bf2
Jaden Padua <japadua@fb.com>
2023-11-07 18:50:56 -0800

    hello cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-4a7d7c6f51e47675.d
@@ -0,0 +1,5 @@
+/Users/japadua/rust/hello-cargo/target/debug/deps/hello_cargo-4a7d7c6f51e47675: src/main.rs
+
+/Users/japadua/rust/hello-cargo/target/debug/deps/hello_cargo-4a7d7c6f51e47675.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/japadua/rust/hello-cargo/target/debug/hello-cargo: /Users/japadua/rust/hello-cargo/src/main.rs
