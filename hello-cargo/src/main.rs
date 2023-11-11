75dd898898f2b86a4dbb6f7a5d2f09f14452edf0
Jaden Padua <paduajaden@gmail.com>
2023-11-11 15:49:32 -0800

    add move and borrow semantics

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,3 +1,19 @@
+fn my_string(s: String) {
+    println!("{}", s);
+}
+
+fn my_string2(s: &String) {
+    println!("{}", s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // move semantics
+    let s1 = String::from("I am s1!");
+    my_string(s1);
+    // println!("{}", s); This will error because s has been moved to my_string
+
+    // borrow semantics
+    let s2: String = String::from("I am s2!");
+    my_string2(&s2);
+    println!("{}", s2); // This will work because s2 is borrowed by my_string2
 }
