4a51f054bc4fc56318f5e6b8b5d90687b4f0865f
Jaden Padua <paduajaden@gmail.com>
2023-11-11 18:44:03 -0800

    Add match and err handling

--- escargo/target/debug/deps/escargo-2e0eadcc93df2e1f.d
+++ escargo/target/debug/deps/escargo-2e0eadcc93df2e1f.d
@@ -1,7 +1,8 @@
-/Users/jadenpadua/rust/escargo/target/debug/deps/escargo-2e0eadcc93df2e1f: src/main.rs src/borrow_semantics.rs src/move_semantics.rs
+/Users/jadenpadua/rust/escargo/target/debug/deps/escargo-2e0eadcc93df2e1f: src/main.rs src/borrow_semantics.rs src/matching.rs src/move_semantics.rs
 
-/Users/jadenpadua/rust/escargo/target/debug/deps/escargo-2e0eadcc93df2e1f.d: src/main.rs src/borrow_semantics.rs src/move_semantics.rs
+/Users/jadenpadua/rust/escargo/target/debug/deps/escargo-2e0eadcc93df2e1f.d: src/main.rs src/borrow_semantics.rs src/matching.rs src/move_semantics.rs
 
 src/main.rs:
 src/borrow_semantics.rs:
+src/matching.rs:
 src/move_semantics.rs:
