4c5066936f7993ff6a9ae8cf43ba8c8c8e447546
Jaden Padua <paduajaden@gmail.com>
2023-11-11 19:54:43 -0800

    simplify logic

--- file_reader/src/file_reader.rs
+++ file_reader/src/file_reader.rs
@@ -5,35 +5,14 @@ use std::io::{BufRead, BufReader};
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     // Collect file path from arguments
-    let file_path: &String = match args.len() {
-        2 => &args[1],
-        _ => panic!("Error: Please only input the file path as your first argument"),
-    };
-    // Open file
-    let file: File = match File::open(file_path) {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            std::io::ErrorKind::NotFound => {
-                panic!("File not found: {}", error)
-            }
-            _ => {
-                panic!("Error opening file: {}", error)
-            }
-        },
-    };
-    // Read file line by line
+    let file_path: &String = args
+        .get(1)
+        .unwrap_or_else(|| panic!("Error: Please only input the file path as your first argument"));
+    let file: File = File::open(file_path).expect("File path should open correctly");
     let reader: BufReader<File> = BufReader::new(file);
+    // Read file line by line
     for line in reader.lines() {
-        match line {
-            Ok(line) => println!("{}", line),
-            Err(error) => match error.kind() {
-                std::io::ErrorKind::InvalidData => {
-                    panic!("Invalid data: {}", error)
-                }
-                _ => {
-                    panic!("Error reading file: {}", error)
-                }
-            },
-        }
+        let line: String = line.expect("Line should be read correctly");
+        println!("{}", line);
     }
 }
